6edea00ba8c5b225850025c26d745082fab64089
akkk33 <akkk33@protonmail.com>
2018-09-22 23:48:30 +0200

    Initial commit

---
+++ __main__.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+"""
+this is my playground for practicing 20 most used python design patterns.
+"""
+__author__ = "Amr Khamis"
+__copyright__ = "Copyright 2018"
+__credits__ = ["Andrew NG"]
+__license__ = "MIT"
+__version__ = "1.0.1"
+__maintainer__ = "Amr Khamis"
+__email__ = "akkk33@pm.me"
+__status__ = "Production"
+
+
+class Main():
+    pass
+
+
+if __name__ == '__main__':  # code to execute if called from command-line
+    pass  # do nothing - code deleted
