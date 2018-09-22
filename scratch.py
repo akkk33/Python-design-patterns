462540faf495e00f6e4416479bcf35f4aa98e738
akkk33 <akkk33@protonmail.com>
2018-09-23 01:42:18 +0200

    Discovered a quick way to make assignments related to previous assignments

--- scratch.py
+++ scratch.py
@@ -11,3 +11,11 @@ __maintainer__ = "Amr Khamis"
 __email__ = "akkk33@pm.me"
 __status__ = "Production"
 
+
+def easter_egg_1():
+    # Shortcut syntax for quick assignments
+    a = [1, 1, 2, 3, 5, 8, 13, 21]
+    even_a = [i for i in a if i % 2 == 0]
+    print(even_a)
+
+
